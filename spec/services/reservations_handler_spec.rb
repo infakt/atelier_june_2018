0c7b3f716bd466a050d99ecba26c5f86e6ba5198
Maciorex <maciej.zabski@gmail.com>
2018-06-18 23:45:57 +0200

    add #reserve action tests

--- spec/services/reservations_handler_spec.rb
+++ spec/services/reservations_handler_spec.rb
@@ -1,5 +1,41 @@
-require "rails/helper"
+require "rails_helper"
 
 RSpec.describe ReservationsHandler, type: :service do
-  
+  let(:user) { User.new }
+  let(:book) { Book.new }
+  subject { described_class.new(user, book) }
+
+  describe '#reserve' do
+
+    before {
+      allow(book).to receive(:can_be_reserved?).with(user).and_return(can_be_reserved)
+  }
+
+    context 'without available book' do
+      let(:can_be_reserved) { false }
+      it {
+        expect(subject.reserve).to eq("Book is not available for reservation")
+      }
+    end
+
+    context 'with available book' do
+      let(:can_be_reserved) { true }
+
+      before {
+        allow(book).to receive_message_chain(:reservations, :create).with(no_args).
+        with(user: user, status: 'RESERVED').and_return(true)
+      }
+
+      it {
+        expect(subject.reserve).to be_truthy
+      }
+    end
+  end
+
+  describe '#take' do
+
+    context 'book cannot be taken' do
+      
+    end
+  end
 end
