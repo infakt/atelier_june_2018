ebd7903497e0f9f58194ccbf5b2f38644fb90aa7
Maciorex <maciej.zabski@gmail.com>
2018-06-20 21:15:54 +0200

    rest api task done

--- app/mailers/book_notifier_mailer.rb
+++ app/mailers/book_notifier_mailer.rb
@@ -1,9 +1,17 @@
 class BookNotifierMailer < ApplicationMailer
   def book_return_remind(book)
     @book = book
-    @reservation = book.reservations.find_by(status: 'TAKEN')
+    @reservation = book.reservations.find_by(status: "TAKEN")
     @user = @reservation.user
 
     mail(to: @user.email, subject: "Oddej mordo tą książke(#{@book.title})!!!!")
+  end
+
+  def book_reserved_return(book)
+    @book = book
+    @reservation = book.reservations.find_by(status: "RESERVED")
+    @user = @reservation.user
+
+    mail(to: @user.email, subject: "Niebawem książka #{@book.title} będzie dostępna!!")
   end
 end
